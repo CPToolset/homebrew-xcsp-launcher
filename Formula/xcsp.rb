fe8cfb2bc437dcf50a5fc541ec026359851eaae8
Anka <runner@Mac-1746548574061.local>
2025-05-06 16:27:48 +0000

    Update formula for version

--- Formula/xcsp.rb
+++ Formula/xcsp.rb
@@ -1,14 +1,14 @@
 class Xcsp < Formula
   desc "Unified launcher for XCSP3 solvers"
   homepage "https://github.com/CPToolset/xcsp-launcher"
-  url "https://github.com/CPToolset/xcsp-launcher/releases/download/v0.1.0a8/xcsp-0.1.0a8-macos.tar.gz"
-  sha256 "773fc63265f191ec75b71d39b3c7c68577658ba306680e3266d1e5246f5c47c7"
+  url "https://github.com/CPToolset/xcsp-launcher/releases/download/v0.1.0a9/xcsp-0.1.0a9-macos.tar.gz"
+  sha256 "6538ba3e7235570dd8c00a305b6d22ae683e15568c26aae920ce1758da70af0c"
   license "LGPL-3.0-or-later"
 
   def install
-    bin.install "xcsp-macos" => "xcsp"
-    share.install "configs" => "xcsp-launcher/configs"
-    share.install "xcsp/tools/xcsp3-solutionChecker-2.5.jar" => "xcsp-launcher/tools/xcsp3-solutionChecker-2.5.jar"
+    bin.install "bin/xcsp-macos" => "xcsp"
+    share.install "share/xcsp-launcher/configs" => "xcsp-launcher/configs"
+    share.install "share/xcsp-launcher/tools/xcsp3-solutionChecker-2.5.jar" => "xcsp-launcher/tools/xcsp3-solutionChecker-2.5.jar"
   end
 
 #  def post_install
