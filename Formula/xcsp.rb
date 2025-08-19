3193b460f965803924b7f35defb7fc312d0006b1
Anka <runner@Mac-1755597485402.local>
2025-08-19 10:02:31 +0000

    Update formula for version

--- Formula/xcsp.rb
+++ Formula/xcsp.rb
@@ -1,8 +1,8 @@
 class Xcsp < Formula
   desc "Unified launcher for XCSP3 solvers"
   homepage "https://github.com/CPToolset/xcsp-launcher"
-  url "https://github.com/CPToolset/xcsp-launcher/releases/download/v0.6.2/xcsp-0.6.2-macos.tar.gz"
-  sha256 "6116bd860881cce2229fc6d003a0a2eec5a78d7585d3c683a74ce0bebd7cbae8"
+  url "https://github.com/CPToolset/xcsp-launcher/releases/download/v0.6.3/xcsp-0.6.3-macos.tar.gz"
+  sha256 "5a6b4889a4d57228010e8d59494cb988c28e4dd1207cc76703900482bc447db6"
   license "LGPL-3.0-or-later"
 
   def install
